225cf4f22e0bc5029907648bb139398bf8c2078e
Starinin Andrey <starininandrey@gmail.com>
2023-07-29 17:39:59 +0300

    Update

---
+++ ConsoleHelper.Lib/TUI.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleHelper.Lib;
+
+public static class TUI
+{
+    #region Print
+
+    private static void Print(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+    public static void PrintError(string message)
+    {
+        Print(message, ConsoleColor.Red);
+    }
+    public static void PrintInfo(string message)
+    {
+        Print(message, ConsoleColor.Blue);
+    }
+
+    #endregion
+
+    #region Input
+
+    public static string InputString(string message)
+    {
+        Console.Write(message);
+        return Console.ReadLine();
+    }
+    public static int InputInt(string message, Action<string>? exceptionHandler = null)
+    {
+        do
+        {
+            try
+            {
+                Console.Write(message);
+                return Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                exceptionHandler?.Invoke("Неправильный ввод! Повторите.");
+            }
+        } while (true);
+    }
+
+    #endregion
+    
+}
---
+++ ConsoleHelper.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleHelper.Lib;
+
+TUI.PrintError("Test");
+TUI.PrintInfo("Test");
+
+var str = TUI.InputString("message: ");
+TUI.PrintInfo($"message = {str}");
+
+var num = TUI.InputInt("number: ");
+TUI.PrintInfo($"number = {num}");
+
+num = TUI.InputInt("number: ", TUI.PrintError);
+TUI.PrintInfo($"number = {num}");
+
+num = TUI.InputInt("number: ", msg =>
+{
+    Console.BackgroundColor = ConsoleColor.Yellow;
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine(msg);
+    Console.ResetColor();
+});
+TUI.PrintInfo($"number = {num}");
